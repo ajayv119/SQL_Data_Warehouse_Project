98352cf671fedf8d2f85dc17e08131dc236b5d51
ajayv119 <96615564+ajayv119@users.noreply.github.com>
2025-12-26 22:56:14 +0530

    Update proc_load_silver.sql

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -198,7 +198,7 @@ BEGIN
 		(cid,cntry)
 
 		SELECT 
-			REPLACE(cid,'','-') cid,
+			REPLACE(cid,'-','') cid,
 			CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
 				 WHEN TRIM(cntry) IN ('US','USA') THEN 'United States'
 				 WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'n/a'
